86392697c18069ad4b176306f2beb5b56977bd22
srira <sriram.bhagavath08@gmail.com>
2026-03-07 19:13:18 +1100

    Created a simple pathfinding algorithm.

--- snake_algo_ai.py
+++ snake_algo_ai.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from pygame.math import Vector2
-
+import heapq
 class SnakeAlgoAI:
     def __init__(self, width, height, cell):
         self.width = width
@@ -39,7 +39,35 @@ class SnakeAlgoAI:
         return self.cycle[(idx + 1) % len(self.cycle)]
 
     def astar(self, snake, apple):
-        pass
+        # snake = [Vector2(start_x, start_y)], a bunch of positions.
+        # apple = Vector2(apple_x, apple_y)
+        # start_pos = snake head: Vector2(x,y)
+        # end_pos = apple: Vector2(a,b)
+        path = [snake[0]]
+        neg_x = False
+        neg_y = False
+        change_x = self.cell
+        change_y = self.cell
+        # calculates horizontal steps based on grid
+        no_of_movements_x = (apple.x - snake[0].x) / self.cell
+        if no_of_movements_x < 0:
+            no_of_movements_x = abs(no_of_movements_x)
+            neg_x = True
+        # calculates vertical steps based on grid
+        no_of_movements_y = (apple.y - snake[0].y) / self.cell
+        if no_of_movements_y < 0:
+            no_of_movements_y = abs(no_of_movements_y)
+            neg_y = True
+        for x in range(int(no_of_movements_x)):
+            if neg_x:
+                change_x = -self.cell
+            path.append(Vector2(path[-1].x + change_x, path[-1].y))
+        for y in range(int(no_of_movements_y)):
+            if neg_y:
+                change_y = -self.cell
+            path.append(Vector2(path[-1].x, path[-1].y + change_y))
+        return path
+
 
     def survival_check(self, snake, path):
         pass
@@ -51,3 +79,9 @@ class SnakeAlgoAI:
         pass
 
 
+ai = SnakeAlgoAI(1000, 500, 25)
+snake = [Vector2(500, 250)]
+apple = Vector2(300, 100)
+path = ai.astar(snake, apple)
+for pos in path:
+    print(pos)
