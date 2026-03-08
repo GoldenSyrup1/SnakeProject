7b11ef6b140df091c7d552bb21751662491aca9c
srira <sriram.bhagavath08@gmail.com>
2026-03-08 13:52:23 +1100

    Fully created greedy best first-search.

--- snake_algo_ai.py
+++ snake_algo_ai.py
@@ -43,33 +43,40 @@ class SnakeAlgoAI:
         # apple = Vector2(apple_x, apple_y)
         # start_pos = snake head: Vector2(x,y)
         # end_pos = apple: Vector2(a,b)
-        path = [snake[0]]
+        snake_head = snake[0]
+        path = [snake_head]
         start = path[-1]
-        neg_x = False
-        neg_y = False
-        change_x = self.cell
-        change_y = self.cell
-        # calculates horizontal steps based on grid
-
-        left = snake[0] + Vector2(-self.cell, 0)
-        right = snake[0] + Vector2(self.cell, 0)
-        up = snake[0] + Vector2(0, -self.cell)
-        down = snake[0] + Vector2(0, self.cell)
-        cardinal_directions = [left, right, up, down]
-        valid_directions = []
-        for i in cardinal_directions:
-            if not ((i.x < 0 or i.x > self.width) or (i.y < 0 or i.y > self.height)) and not (i in snake) and not(i in path):
-                valid_directions.append(i)
-        scores = {(int(v.x), int(v.y)): 0 for v in valid_directions}
-        for j in scores.keys():
-            no_of_movements_x = abs((apple.x - j[0]) / self.cell)
-            no_of_movements_y = abs((apple.y - j[1]) / self.cell)
-            h = no_of_movements_x + no_of_movements_y
-            g = 1
-            scores[j] = g+h
-            pass
-        # For now, return none
-        return None
+        while (start != apple):
+            # create 4 cardinal directions
+            left = start + Vector2(-self.cell, 0)
+            right = start + Vector2(self.cell, 0)
+            up = start + Vector2(0, -self.cell)
+            down = start + Vector2(0, self.cell)
+            cardinal_directions = [left, right, up, down]
+            valid_directions = []
+            # check if there is anything they have explored or interefered with
+            for i in cardinal_directions:
+                if not ((i.x < 0 or i.x >= self.width) or (i.y < 0 or i.y >= self.height)) and not (
+                        i in snake) and not (i in path):
+                    valid_directions.append(i)
+            if not valid_directions:
+                return None
+            scores = {(int(v.x), int(v.y)): 0 for v in valid_directions}
+            # find best one
+            for j in scores.keys():
+                no_of_movements_x = abs((apple.x - j[0]) / self.cell)
+                no_of_movements_y = abs((apple.y - j[1]) / self.cell)
+                h = no_of_movements_x + no_of_movements_y
+                g = len(path)
+                scores[j] = g + h
+            best_tuple = min(scores, key=lambda k: scores[k])
+            best_vector = Vector2(*best_tuple)
+            # add it to path list
+            path.append(best_vector)
+            # make the start the best node now for another good node in the future
+            start = best_vector
+        # return path
+        return path
 
 
     def survival_check(self, snake, path):
@@ -83,7 +90,7 @@ class SnakeAlgoAI:
 
 
 ai = SnakeAlgoAI(1000, 500, 25)
-snake = [Vector2(500, 250)]
+snake = [Vector2(500, 250), Vector2(475, 250), Vector2(450, 250)]
 apple = Vector2(300, 100)
 path = ai.astar(snake, apple)
 for pos in path:
